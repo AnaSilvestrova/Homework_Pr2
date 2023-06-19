b8a3603c34b3a813d288669b6232cd234439d737
AnaSilvestrova <silvestrovaab@gmail.com>
2023-06-20 03:42:27 +0700

    task 10 void used

---
+++ Task10_Void/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход СЛУЧАЙНОЕ трёхзначное число и на выходе показывает вторую цифру этого числа.
+*/
+
+void GetSecNumber()
+{
+    int number = new Random().Next(100, 1000);
+
+    Console.Write(number + " => ");
+    
+    int result = (number / 10) % 10;
+    
+    Console.WriteLine($"The second digit of {number} is {result}.");
+}
+
+GetSecNumber();
