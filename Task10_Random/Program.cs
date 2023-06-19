cd5ef4bdc48e6236b54cf2d75b026ec80d8841ed
AnaSilvestrova <silvestrovaab@gmail.com>
2023-06-20 03:35:43 +0700

    random number used

---
+++ Task10_Random/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход СЛУЧАЙНОЕ трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Random rand = new Random();
+int number = rand.Next(100,1000);
+int result = (number / 10) % 10;
+Console.WriteLine($"The second digit of {number} is {result}.");
