8f0c42abf11f1d316ee59a2120640c75875741ab
AnaSilvestrova <silvestrovaab@gmail.com>
2023-06-20 04:05:28 +0700

    task 13 added

---
+++ Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.WriteLine("Input number --> ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"The third digit of {num} is {result}.");
+
+
+    Console.WriteLine($"Error, there is no third digit.");
